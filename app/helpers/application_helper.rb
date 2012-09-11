297f1030c84d40804e4e0273046c490f31b7f0f8
Siwei Shen <sg552sg552@gmail.com>
2012-09-11 16:44:55 +0800

    added more acupuncture points to the map and extracted the partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
   # options:  must give the :coordinates and :title
   def point_area(options = {} )
+    options = options.reverse_merge(:radius => 10)
     raw %Q{
-<area coords='#{options[:coordinates]},7'
+<area coords='#{options[:coordinates]},#{options[:radius]}'
   data-maphilight='{"strokeColor":"ff0000","strokeWidth":4,"fillColor":"9966ff","fillOpacity":0.0}'
   href='#'
   onclick='add_to_list(#{options[:point_id]});return false'
